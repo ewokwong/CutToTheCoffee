be563fb0826a1d1515e3460eb51dd78715020aa1
Ethan Kwong <eonnkwong@atlassian.com>
2025-10-12 10:53:42 +1100

    Initial Commit

---
+++ Cut to the Coffee/Cut_to_the_CoffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cut_to_the_CoffeeApp.swift
+//  Cut to the Coffee
+//
+//  Created by Ethan Kwong on 12/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Cut_to_the_CoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
